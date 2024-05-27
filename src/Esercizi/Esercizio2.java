31b2f6fbd7cbdc94b67794b27d5dfe50afcc20d6
FocusFine89 <niivanov02@gmail.com>
2024-05-27 16:26:59 +0200

    add exception in esercizio2

--- src/Esercizi/Esercizio2.java
+++ src/Esercizi/Esercizio2.java
@@ -8,12 +8,14 @@ public class Esercizio2 {
         while (true) {
             try {
                 System.out.println("Quanti litri hai messo di carburante:");
-                double litri = Double.parseDouble(sc.nextLine());
+                int litri = (int) Double.parseDouble(sc.nextLine());
                 System.out.println("Quanti km hai percorso con l'auto?");
                 int km = Integer.parseInt(sc.nextLine());
                 System.out.println("Hai consumato " + km / litri + " l/km");
             } catch (ArithmeticException ex) {
                 System.out.println("Non puoi dividere per zero, stronzo");
+            } catch (NumberFormatException ex) {
+                System.out.println("Non hai inserito un numero o il numero è troppo alto ");
             }
 
 
